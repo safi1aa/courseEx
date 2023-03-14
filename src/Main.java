39bfa1c98d732aa6ab94855d7f445d6e9b8fe046
User <egemberdievasafia@gmail.com>
2023-03-14 20:59:12 +0600

    students commit

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         Course courseEx = new Course("autsa", 8,"azamat agai",10);
+        Student student = new Student("26.10.2008","Safia","Egemberdieva",14);
 
+        System.out.println(student);
         System.out.println(courseEx);
+
 
     }
 }
--- src/Student.java
+++ src/Student.java
@@ -4,4 +4,24 @@ public class Student {
     String surname;
     int age;
 
+    public Student(String birthday, String name, String surname, int age) {
+        this.birthday = birthday;
+        this.name = name;
+        this.surname = surname;
+        if (age > 0) {
+            this.age = age;
+        }
+    }
+
+    public Student() {
+    }
+
+    @Override
+    public String toString() {
+        return "Student\n" +
+                "birthday: " + birthday + "\n" +
+                " name: " + name + "\n" +
+                "surname: " + surname + "\n" +
+                " age: " + age;
+    }
 }
